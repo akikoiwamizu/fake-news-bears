8819f49da56e1de66259137e9a797fe7cec9ab0d
akikoiwamizu <akikoiwamizu@gmail.com>
2023-01-31 18:37:49 +0100

    Applying black formatting

--- samples/get_tweets_random.py
+++ samples/get_tweets_random.py
@@ -17,7 +17,7 @@ def create_url():
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    ids = randrange(100000000,1935870145,3)
+    ids = randrange(100000000, 1935870145, 3)
     # You can adjust ids to include a single Tweets.
     # Or you can add to up to 100 comma-separated IDs
     url = "https://api.twitter.com/2/tweets?ids={}&{}".format(ids, tweet_fields)
@@ -39,19 +39,16 @@ def connect_to_endpoint(url):
     print(response.status_code)
     if response.status_code != 200:
         raise Exception(
-            "Request returned an error: {} {}".format(
-                response.status_code, response.text
-            )
+            "Request returned an error: {} {}".format(response.status_code, response.text)
         )
     return response.json()
 
 
 def main():
-	for i in range(500):
-	    url = create_url()
-	    json_response = connect_to_endpoint(url)
-	    print(json.dumps(json_response, indent=4, sort_keys=True))
-
+    for i in range(500):
+        url = create_url()
+        json_response = connect_to_endpoint(url)
+        print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
 if __name__ == "__main__":
