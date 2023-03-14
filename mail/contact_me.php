d6158bc8e159feb3d0d5f9d338e6f1cebbd6bbc7
akikoiwamizu <akikoiwamizu@gmail.com>
2023-03-14 21:45:27 +0100

    Debugging contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = "aiwamizu@berkeley.edu"; // This is where the form will send a message to.
+$to = 'aiwamizu@berkeley.edu'; // This is where the form will send a message to.
 $email_subject = "Capstone Contact Form: $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: aiwamizu@berkeley.edu\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
